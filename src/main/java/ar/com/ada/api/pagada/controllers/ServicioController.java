5c2530f87525e8323d5d840282f181d70c2ff0ce
alexmarygr <alexmarygr@gmail.com>
2020-08-15 12:35:56 -0300

    Crear el metodo crearServicio en el controller

---
+++ src/main/java/ar/com/ada/api/pagada/controllers/ServicioController.java
@@ -0,0 +1,73 @@
+package ar.com.ada.api.pagada.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.com.ada.api.pagada.entities.Servicio;
+import ar.com.ada.api.pagada.models.request.ServicioRequest;
+import ar.com.ada.api.pagada.models.response.GenericResponse;
+import ar.com.ada.api.pagada.services.DeudorService;
+import ar.com.ada.api.pagada.services.EmpresaService;
+import ar.com.ada.api.pagada.services.ServicioService;
+import ar.com.ada.api.pagada.services.ServicioService.ServicioValidacionEnum;
+
+@RestController
+public class ServicioController {
+
+    @Autowired
+    ServicioService servicioService;
+
+    @Autowired
+    EmpresaService empresaService;
+
+    @Autowired
+    DeudorService deudorService;
+
+    @PostMapping("/api/servicios")
+    public ResponseEntity<GenericResponse> crearServicio(@RequestBody ServicioRequest sr){
+
+        GenericResponse gr = new GenericResponse();
+
+        Servicio servicio = new Servicio();
+        servicio.setEmpresa(empresaService.buscarEmpresaPorId(sr.empresaId));
+        servicio.setDeudor(deudorService.buscarDeudorPorId(sr.deudorId));
+        servicio.setTipoServicio(servicioService.buscarTipoServicioPorId(sr.tipoServicioId));
+        servicio.setTipoComprobante(sr.tipoComprobanteId);
+        servicio.setNumero(sr.numero);
+        servicio.setFechaEmision(sr.fechaEmision);
+        servicio.setFechaVencimiento(sr.fechaVencimiento);
+        servicio.setImporte(sr.importe);
+        servicio.setMoneda(sr.moneda);
+        servicio.setCodigoBarras(sr.codigoBarras);
+        servicio.setEstadoId(sr.estadoId);
+
+        ServicioValidacionEnum validacionResultado = servicioService.validarServicio(servicio);
+        if(validacionResultado != ServicioValidacionEnum.OK){
+            gr.isOk = false;
+            gr.message = "Hubo un error en la validacion del servicio"  + validacionResultado;
+            return ResponseEntity.badRequest().body(gr);
+        }
+
+        servicioService.crearServicio(servicio);
+        //Quiero ver si en la base de datos se creó el id
+        if (servicio.getServicioId() == null){
+
+            gr.isOk = false;
+            gr.message = "No se pudo crear el servicio";
+            return ResponseEntity.badRequest().body(gr);
+
+        } else{
+
+            gr.isOk = true;
+            gr.id = servicio.getServicioId();
+            gr.message = "Se creó el servicio éxitosamente.";
+            return ResponseEntity.ok(gr);
+
+        }
+        
+    }
+
+}
